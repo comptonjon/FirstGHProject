a53f8fdd9048c333fe534b0060864cb10040eaf0
Jonathan Compton <jonathankcompton@gmail.com>
2018-11-21 14:42:13 -0800

    Complete first "starter" project

--- FirstGHProject/ViewController.swift
+++ FirstGHProject/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var revealOrHideButton: UIButton!
+    
+    var labelIsHidden = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        messageLabel.alpha = 0
     }
 
-
+    @IBAction func revealOrHideButtonTapped(_ sender: Any) {
+        if labelIsHidden {
+            UIView.animate(withDuration: 0.3) {
+                self.messageLabel.alpha = 1.0
+            }
+        } else {
+            UIView.animate(withDuration: 0.3) {
+                self.messageLabel.alpha = 0.0
+            }
+        }
+        labelIsHidden = !labelIsHidden
+        setButtonTitle()
+        
+    }
+    
+    func setButtonTitle() {
+        let title = labelIsHidden ? "Reveal \"Surprise\"" : "Hide Message"
+        revealOrHideButton.setTitle(title, for: .normal)
+    }
+    
 }
 
